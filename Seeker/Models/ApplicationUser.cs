39c5ae72871872e5e9639e1597f9f1659250cb25
Thilina Wanniarachchi <thilinaw@jrcsoftware.com>
2020-02-12 00:41:04 +0530

    back end

---
+++ Seeker/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+////using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Seeker.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Column(TypeName = "nvarchar(150)")]
+		public string FullName { get; set; }
+	}
+}
